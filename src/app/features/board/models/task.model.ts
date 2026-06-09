b655c3cc505ad8b181c4f094bc19d577a5012524
Sebastian Enriquez <sebasweb.devx@gmail.com>
2026-06-09 12:31:07 -0600

    feat(models): add task and subtask interfaces and enums

---
+++ src/app/features/board/models/task.model.ts
@@ -0,0 +1,30 @@
+export enum Priority {
+    Alta = 'alta',
+    Media = 'media',
+    Baja = 'baja'
+}
+
+ export enum TaskStatus {
+    Todo = 'todo',
+    InProgress = 'inprogress',
+    ToRevision = 'torevision',
+    Completed = 'completed'
+}
+
+export interface Task{
+    id: string;
+    title: string;
+    description: string;
+    priority: Priority;
+    status: TaskStatus;
+    labels: string[];
+    date: Date;
+    subtask?: Subtask[];
+    assigned?: string[];
+    comments?: number;
+}
+
+export interface Subtask{
+    text: string;
+    completed: boolean;
+}

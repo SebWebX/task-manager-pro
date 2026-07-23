c441c1255980d4c07d06adf75cc91050256d7082
Sebastian Enriquez <sebasweb.devx@gmail.com>
2026-07-22 21:19:22 -0600

    feat(board-controls): add board controls scss styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 import { TopbarComponent } from './shared/components/topbar/topbar.component';
+import { BoardControlsComponent } from './features/board/board-controls/board-controls.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent, TopbarComponent],
+  imports: [RouterOutlet, SidebarComponent, TopbarComponent, BoardControlsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/features/board/board-controls/board-controls.component.ts
+++ src/app/features/board/board-controls/board-controls.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-board-controls',
   imports: [],
   templateUrl: './board-controls.component.html',
-  styleUrl: './board-controls.component.scss'
+  styleUrl: './board-controls.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class BoardControlsComponent {
 

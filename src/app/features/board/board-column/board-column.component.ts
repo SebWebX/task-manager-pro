97277b5912f5d819af60e3b140b025b856efeeb2
Sebastian Enriquez <sebasweb.devx@gmail.com>
2026-07-23 21:36:47 -0600

    feat(board-column): add board column html structure with bem and aria

--- src/app/features/board/board-column/board-column.component.ts
+++ src/app/features/board/board-column/board-column.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Column } from '../models/task.model';
+import { TaskCardComponent } from '../task-card/task-card.component';
 
 @Component({
   selector: 'app-board-column',
-  imports: [],
+  imports: [TaskCardComponent],
   templateUrl: './board-column.component.html',
   styleUrl: './board-column.component.scss'
 })

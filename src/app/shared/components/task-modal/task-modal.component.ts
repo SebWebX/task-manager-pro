58ea0563196da7b2e4c6a6b455d7eb729a1fa217
Sebastian Enriquez <sebasweb.devx@gmail.com>
2026-07-28 21:49:07 -0600

    feat(task-modal): add reactive form and task creation functionality

--- src/app/shared/components/task-modal/task-modal.component.ts
+++ src/app/shared/components/task-modal/task-modal.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { ModalService } from '../../../core/services/modal.service';
 import { Subscription } from 'rxjs';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { TaskService } from '../../../core/services/task.service';
+import { Task, Priority, TaskStatus } from '../../../features/board/models/task.model';
 
 
 @Component({
   selector: 'app-task-modal',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './task-modal.component.html',
   styleUrl: './task-modal.component.scss',
   encapsulation: ViewEncapsulation.None
@@ -14,8 +17,15 @@ export class TaskModalComponent implements OnInit, OnDestroy {
   isOpen: boolean = false;
   private subscription!: Subscription;
 
+  taskForm = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    description: new FormControl(''),
+    priority: new FormControl('alta', [Validators.required]),
+    date: new FormControl('')
+  });
 
-  constructor(private modalService: ModalService){}
+
+  constructor(private modalService: ModalService, private taskService: TaskService) {}
 
 
   ngOnInit(): void{
@@ -31,4 +41,26 @@ export class TaskModalComponent implements OnInit, OnDestroy {
   closeModal(): void {
     this.modalService.closeModal();
   }
+
+  onSubmit(): void{
+    if (this.taskForm.valid){
+      const formValue = this.taskForm.value;
+
+      const newTask: Task ={
+        id: Date.now().toString(),
+        title: formValue.title!,
+        description: formValue.description || '',
+        priority: formValue.priority as Priority,
+        status: TaskStatus.Todo,
+        labels: [],
+        date: formValue.date ? new Date(formValue.date) : new Date()
+      };
+
+      this.taskService.addTask(newTask, '01');
+      this.taskForm.reset({ priority: 'alta'});
+      this.closeModal();
+    }
+  }
+
+  
 }
